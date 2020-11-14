ea919b66bed17e1d8bcb05e562332912800f1828
xwaxes01 <xwaxes@gmail.com>
2020-11-14 11:39:40 +0300

    Optimized notification building and improved notification look and style

--- app/src/main/java/com/example/tuchatrcsmessenger/FCMClass.java
+++ app/src/main/java/com/example/tuchatrcsmessenger/FCMClass.java
@@ -8,13 +8,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
+import android.media.RingtoneManager;
 import android.os.Build;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 
 import com.example.tuchatrcsmessenger.Classes.SaveTokenObject;
+import com.example.tuchatrcsmessenger.External.TinyDB;
 import com.example.tuchatrcsmessenger.data.db.AppDatabase;
 import com.example.tuchatrcsmessenger.data.entity.LastMessage;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -29,32 +35,37 @@ import com.google.firebase.messaging.RemoteMessage;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 
 public class FCMClass extends FirebaseMessagingService {
+    ArrayList<String> mSet = new ArrayList<>();
+    private SharedPreferences mSharedpreferences;
+    private TinyDB mTinyDB;
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+
+        mSharedpreferences = getSharedPreferences("pref", Context.MODE_PRIVATE);
+
+        mTinyDB = new TinyDB(this);
+
         Log.d("MessagingService", "onMessageReceived Called");
-
-
-        String identifyDataType =
-                remoteMessage.getData().get("data_type");
+        String identifyDataType = remoteMessage.getData().get("data_type");
 
         if (Objects.equals(identifyDataType, "data_type_chat_message")) {
+            // Save message details
             String title = remoteMessage.getData().get("title");
-
             String message = remoteMessage.getData().get("message");
-
             String sender = remoteMessage.getData().get("sender_name");
-
             String sender_id = remoteMessage.getData().get("sender_id");
-
             String chatRoomId = remoteMessage.getData().get("chatRoom_id");
-
             String sentTime = remoteMessage.getData().get("sentTime");
 
             char[] chars = sentTime.toCharArray();
@@ -76,8 +87,7 @@ public class FCMClass extends FirebaseMessagingService {
 
                 LastMessage lastMessage = new LastMessage(message, formatter.format(strDate), chatRoomId, 1);
 
-
-                //Build notification
+                //Build notification if conversation is not open or app is not open
                 if (ChatsActivity.chatRoomId == null) {
                     saveLastMessage(lastMessage, chatRoomId);
                     buildNotification(title, message, sender, sender_id, chatRoomId);
@@ -97,30 +107,36 @@ public class FCMClass extends FirebaseMessagingService {
     }
 
     private void saveLastMessage(LastMessage lastMessage, String chatRoomId) {
-
         AppDatabase db = AppDatabase.getInstance(this);
-
         int existingUnreadCount = db.getLastMessageDao().getLastMessage(chatRoomId).getUnreadCount();
-
         lastMessage.setUnreadCount(existingUnreadCount + 1);
-
         db.getLastMessageDao().insertLastMessage(lastMessage);
-
     }
 
 
     private void buildNotification(String title, String message, String sender, String sender_id, String chatRoomId) {
+        saveNotifMessages(chatRoomId, message);
 
+        // Create intent
         Intent intent = new Intent(this, ChatsActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
-        int notificationId = buildNotificationId(chatRoomId);
-        saveNotifID(notificationId, chatRoomId);
         intent.putExtra("Sender Name", sender);
         intent.putExtra("id", sender_id);
         intent.putExtra("Chat ID", chatRoomId);
         intent.putExtra("open_main_activity", true);
 
+        // Create list to save multiple messages
+        ArrayList<String> unreadMessages = mTinyDB.getListString(chatRoomId);
+
+        Log.d("Set", unreadMessages+" end ");
+
+        // Create and save notification id
+        int notificationId = buildNotificationId(chatRoomId);
+
+
+        saveNotifID(notificationId, chatRoomId);
+
+        // Create pending intent
         PendingIntent notifyPendingIntent = PendingIntent.getActivity(
                 this,
                 0,
@@ -128,87 +144,110 @@ public class FCMClass extends FirebaseMessagingService {
                 PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT
         );
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationManager notificationManager =
-                    (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        // Create notification manager
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-            String channelId = "channel-01";
-            String channelName = "Chat Channel";
+        String channelId = "channel-01";
+        String channelName = "Chat Channel";
+
+        // Build notification channel for android 8+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
 
-            NotificationChannel mChannel = new NotificationChannel(
-                    channelId, channelName, importance
-            );
-            if (notificationManager != null) {
-                notificationManager.createNotificationChannel(mChannel);
-            }
+            NotificationChannel mChannel = new NotificationChannel(channelId, channelName, importance);
 
-            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, channelId)
-                    .setDefaults(Notification.DEFAULT_ALL)
-                    .setContentTitle(sender)
-                    .setStyle(
-                            new NotificationCompat
-                                    .BigTextStyle()
-                                    .bigText(message)
-                                    .setBigContentTitle(sender)
-                                    .setSummaryText("New message")
-                    )
-                    .setSmallIcon(R.mipmap.ic_launcher)
-                    .setLargeIcon(
-                            BitmapFactory.decodeResource(
-                                    getApplicationContext().getResources(),
-                                    R.mipmap.ic_launcher
-                            )
-                    )
-                    .setOnlyAlertOnce(true)
-                    .setAutoCancel(true);
-
-            mBuilder.setContentIntent(notifyPendingIntent);
-
-            if (notificationManager != null) {
-                notificationManager.notify(notificationId, mBuilder.build());
-            }
-
-        } else {
-
-            // Instantiate a Builder object.
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(
-                    this, "default_notification_channel_name"
-            ).setDefaults(Notification.DEFAULT_ALL)
-                    .setContentTitle(sender)
-                    .setStyle(
-                            new NotificationCompat
-                                    .BigTextStyle()
-                                    .bigText(message)
-                                    .setBigContentTitle(sender)
-                                    .setSummaryText("New message")
-                    )
-                    .setSmallIcon(R.mipmap.ic_launcher)
-                    .setLargeIcon(
-                            BitmapFactory.decodeResource(
-                                    getApplicationContext().getResources(),
-                                    R.mipmap.ic_launcher
-                            )
-                    )
-                    .setOnlyAlertOnce(true)
-                    .setAutoCancel(true);
-            builder.setContentIntent(notifyPendingIntent);
-            NotificationManager mNotificationManager =
-                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
             if (mNotificationManager != null) {
-                mNotificationManager.notify(notificationId, builder.build());
+                mNotificationManager.createNotificationChannel(mChannel);
             }
+
         }
 
+        // Return unread messages
+        int messagesCount = unreadMessages.size();
 
+
+
+        // Create notification builder
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, channelId)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setContentTitle(sender)
+                .setContentText(message)
+                .setStyle(new NotificationCompat
+                                .BigTextStyle()
+                                .bigText(message)
+                                .setBigContentTitle(sender +": " +message))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(
+                        BitmapFactory.decodeResource(
+                                getApplicationContext().getResources(),
+                                R.mipmap.ic_launcher
+                        )
+                )
+                .setOnlyAlertOnce(true)
+                .setAutoCancel(true);
+
+        // Create inbox-style notifications
+        Notification mBuilderInbox;
+        if (messagesCount > 3) {
+            mBuilderInbox = new NotificationCompat.InboxStyle(mBuilder)
+                    .addLine(unreadMessages.get(0))
+                    .addLine(unreadMessages.get(1))
+                    .addLine(unreadMessages.get(2) +" " +"(+" +(messagesCount-3) +" unread)")
+                    .setBigContentTitle(sender)
+                    .setSummaryText(messageCounter(messagesCount))
+                    .build();
+        } else if (messagesCount == 3){
+            mBuilderInbox = new NotificationCompat.InboxStyle(mBuilder)
+                    .addLine(unreadMessages.get(0))
+                    .addLine(unreadMessages.get(1))
+                    .addLine(unreadMessages.get(2))
+                    .setBigContentTitle(sender)
+                    .setSummaryText(messageCounter(messagesCount))
+                    .build();
+        } else if (messagesCount == 2){
+            mBuilderInbox = new NotificationCompat.InboxStyle(mBuilder)
+                    .addLine(unreadMessages.get(0))
+                    .addLine(unreadMessages.get(1))
+                    .setBigContentTitle(sender)
+                    .setSummaryText(messageCounter(messagesCount))
+                    .build();
+        } else {
+            mBuilderInbox = new NotificationCompat.InboxStyle(mBuilder)
+                    .addLine(unreadMessages.get(0))
+                    .setBigContentTitle(sender)
+                    .setSummaryText(messageCounter(messagesCount))
+                    .build();
+        }
+
+        mBuilder.setContentIntent(notifyPendingIntent);
+
+        if (mNotificationManager != null) {
+            mNotificationManager.notify(notificationId, mBuilderInbox);
+        }
+    }
+
+    public String messageCounter(int messageCount) {
+        if (messageCount > 1) return messageCount +" new messages";
+        else return messageCount +" new message";
     }
 
     private void saveNotifID(int notificationId, String chatRoomId) {
-        SharedPreferences sharedpreferences = getSharedPreferences("pref", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedpreferences.edit();
-
+        SharedPreferences.Editor editor = mSharedpreferences.edit();
         editor.putInt(chatRoomId, notificationId);
         editor.apply();
+    }
+
+    private void saveNotifMessages(String chatRoomId, String message){
+        // Get from shared prefs using TinyDB
+        mSet = mTinyDB.getListString(chatRoomId);
+
+        // Add latest message
+        mSet.add(message);
+
+        // Save to shared pref using TinyDB again
+        mTinyDB.putListString(chatRoomId, mSet);
     }
 
     @Override
@@ -233,7 +272,6 @@ public class FCMClass extends FirebaseMessagingService {
             }
         });
     }
-
 
     private void sendRegistrationToServer(String deviceToken) {
 
@@ -272,4 +310,6 @@ public class FCMClass extends FirebaseMessagingService {
         }
         return notificationId;
     }
+
+
 }
