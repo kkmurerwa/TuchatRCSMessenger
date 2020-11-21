03d480d08df851af463626d440556283bfd9d94c
xwaxes01 <xwaxes@gmail.com>
2020-11-21 18:31:29 +0300

    Made changes to the app settings activity

--- app/src/main/java/com/example/tuchatrcsmessenger/SettingsActivity.java
+++ app/src/main/java/com/example/tuchatrcsmessenger/SettingsActivity.java
@@ -1,22 +1,50 @@
 package com.example.tuchatrcsmessenger;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.tuchatrcsmessenger.External.CustomProgress;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+
+    private TextInputEditText mEditTextName;
+    private TextInputEditText mEditTextPhone;
+
+    private TextInputLayout mTextFieldName;
+    private TextInputLayout mTextFieldPhone;
+
+    private Button mButtonSaveUserName;
+    private Button mButtonSaveUserPhone;
+
+    //Firebase path variables
+    final String userCollection = "users";
+
+    private String mUserId;
+    private FirebaseFirestore mDb;
+    private ProgressDialog mProgressDialog;
+    private CustomProgress mCustomProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +57,13 @@ public class SettingsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setMessage("Loading user info");
+        mProgressDialog.show();
+
+//        mCustomProgress = CustomProgress.getInstance();
+//        mCustomProgress.showProgress(this, "Loading stuff", false);
+
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,6 +73,52 @@ public class SettingsActivity extends AppCompatActivity {
 
         //Initialize firebase auth
         mFirebaseAuth = FirebaseAuth.getInstance();
+        mUserId = mFirebaseAuth.getUid();
+
+        // Initialize text input edit texts
+        mEditTextName = findViewById(R.id.text_input_user_name);
+        mEditTextPhone = findViewById(R.id.text_input_user_phone);
+
+        // Initialize text fields
+        mTextFieldName = findViewById(R.id.text_field_user_name);
+        mTextFieldPhone = findViewById(R.id.text_field_user_phone);
+
+        // Initialize save buttons
+        mButtonSaveUserName = findViewById(R.id.button_save_user_name);
+        mButtonSaveUserPhone = findViewById(R.id.button_save_user_phone);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //Do stuff
+        retrieveUserName();
+    }
+
+    private void retrieveUserName(){
+        //Initialize Firebase Firestore Instance
+        mDb = FirebaseFirestore.getInstance();
+
+        mDb.collection(userCollection)
+                .document(mUserId)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        String userName = documentSnapshot.getString("user_name");
+                        String userPhone = documentSnapshot.getString("user_phone");
+                        mEditTextName.setText(userName);
+                        mEditTextPhone.setText(userPhone);
+                        mProgressDialog.dismiss();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        mProgressDialog.dismiss();
+                        Toast.makeText(SettingsActivity.this, "Could not retrieve user info", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     public void buttonClickManager(View v) {
@@ -75,6 +156,15 @@ public class SettingsActivity extends AppCompatActivity {
                     Snackbar.make(parentLayout, "Could not sign you out", Snackbar.LENGTH_LONG).show();
                 }
                 break;
+//            case R.id.text_input_user_name:
+//                mEditTextName.setFocusable(true);
+//                mEditTextName.requestFocus();
+//                mButtonSaveUserName.setVisibility(View.VISIBLE);
+//                break;
+//            case R.id.text_input_user_phone:
+//                mEditTextPhone.setFocusable(true);
+//                mButtonSaveUserPhone.setVisibility(View.VISIBLE);
+//                break;
             default:
                 break;
         }
